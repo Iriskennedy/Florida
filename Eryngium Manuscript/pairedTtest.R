6c2a5d327a05493fe802902254cc1ee302bcdde0
Iris Kennedy <ik3zq@virginia.edu>
2024-05-22 17:15:02 -0400

    Update pairedTtest.R
    ran paired t test on scrub near far, did not find significance

--- Eryngium Manuscript/pairedTtest.R
+++ Eryngium Manuscript/pairedTtest.R
@@ -8,13 +8,24 @@ scrubData <- read_csv("rosemaryBaldsCombined.csv")
 scrubData<- subset(scrubData, bald != 45) #taking out bald 45 because there were no seeds found
 
 scrubSub <- scrubData%>%
-  select(transectNum, nearFar, seedAbundance, abundanceMass)%>%
-  mutate(transectNum= as.character(transectNum))
+  select(transectNum, nearFar, seedAbundance)%>%
+  mutate(transectNum= as.factor(transectNum),
+         nearFar=as.factor(nearFar))
 
 #reshape to have columns for transectNum, N, F and Seed abundance
 
-newdf <- reshape(scrubSub, direction = "wide", idvar = "transectNum", timevar = "nearFar")
+newdf <- pivot_wider(scrubSub, names_from = nearFar, values_from = seedAbundance)
 
-t.test(scrubSub$nearFar)
+t.test(newdf$N, newdf$F)
 
 t.test(scrubData$seedAbundance, fig1Data$seedAbundance)
+
+#data:  newdf$N and newdf$F
+#t = -0.43683, df = 45.143, p-value = 0.6643
+
+#checking assumptions
+# compute the difference, we need to check whether the differences of the pairs follow a normal distribution.
+d <- with(my_data, 
+          weight[group == "before"] - weight[group == "after"])
+# Shapiro-Wilk normality test for the differences
+shapiro.test(d) # => p-value = 0.6141
